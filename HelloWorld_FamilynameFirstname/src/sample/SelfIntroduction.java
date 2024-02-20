fbe98f5b8e021901a41899dd7dd27d7ebd36c860
user <user@W-UEDAKEITA-X>
2024-02-20 12:26:30 +0900

    JAVA導入（自己紹介）

---
+++ HelloWorld_FamilynameFirstname/src/sample/SelfIntroduction.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//氏名
+		String name = "山田 花子";
+		//年齢
+		int age = 25;
+		//趣味
+		String hobby = "映画鑑賞";
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
