3453d18a0396691bdb4d1bd90bf685b9bbf4b7b1
user <user@W-UEDAKEITA-X>
2024-03-12 06:32:45 +0900

    1.22 パッケージをコンパイル

---
+++ HelloJapan_UedaKeita/src/execution/Execution.java
@@ -0,0 +1,32 @@
+package execution;
+
+public class Execution {
+
+	// フィールドに国の名前の変数を定義
+	public String countryName;
+	
+	//　フィールドに料理の名前の変数を定義
+	public String foodName;
+	
+	// フィールドに料理のジャンルの変数を定義
+	public String foodGenre;
+
+	
+	// 変数countyNameに「日本」を代入するコンストラクタを作成
+	public void setCountｒy() {
+		this.countryName = "日本";
+	}
+	
+	// 変数countyNameに「寿司」を代入するコンストラクタを作成
+	public void setFood() {
+		this.foodName = "寿司";
+	}
+	
+	// 変数foodGenreに「和食」を代入するコンストラクタを作成
+	public void setGenre() {
+		this.foodGenre = "和食";
+	}
+	
+	
+	
+}
