d489cd9d27cc5b5f320e0cdbf98a82657dff9bed
user <user@W-UEDAKEITA-X>
2024-03-20 21:20:24 +0900

    1.32 自己紹介プログラムを作成③

---
+++ self_introduction3_UedaKeita/src/person/Person.java
@@ -0,0 +1,59 @@
+package person;
+
+public class Person{
+	
+	
+	// クラスフィールド値countを定義
+	public static int count = 0;
+	
+	// フィールド値の定義
+	public String firstName;
+	public String lastName;
+	public int age;
+	public double height, weight;
+
+	// コンストラクタを定義しインスタンスフィールドに値をセット
+	public Person(String firstName, int age, double height, double weight){
+		// 引数をフィールド値に代入
+		this.firstName = firstName;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		// countの値に1足す
+		Person.count++;
+	}
+	
+	// Personメソッドをオーバーロード。引数にlastNameを追加
+	public Person(String firstName, String lastName, int age, double height, double weight){
+		// 引数をフィールド値に代入
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		// countの値に1足す
+		Person.count++;
+	}
+
+	// フルネームを取得するクラスメソッド
+	public String fullName(){
+		return this.firstName + this.lastName;
+	}
+
+	// 名前と年齢を出力するメソッド
+	public void print(){
+		System.out.println("名前は" + this.fullName() + "です");
+		System.out.println("年は" + this.age + "です");
+	}
+
+	// bmiを返すメソッド
+	public double bmi(){
+		return this.weight / this.height / this.height;
+	}
+
+	// 合計人数を出力するクラスメソッド
+	public static void printCount(){
+		System.out.println("合計" + Person.count + "人です");
+	}
+	
+}
